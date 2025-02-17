3529891310aaec1ca457e445cb24c6df554d5bb2
caoyingkui <caoyingkui@kuaishou.com>
2025-02-17 20:08:42 +0800

    some change

--- item/urls.py
+++ item/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product", views.product, name="product"),
+    path("cart", views.cart, name="cart"),
 ]
--- item/views.py
+++ item/views.py
@@ -10,3 +10,13 @@ def index(request):
     template = loader.get_template("item/index.html")
     context = {}
     return HttpResponse(template.render(context, request))
+
+def product(request):
+    template = loader.get_template("item/product.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def cart(request):
+    template = loader.get_template("item/cart.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
